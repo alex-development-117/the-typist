a978f7311091cacc4679e859ce23cecd320e2947
Alejandro QF <tnt_games_development@outlook.com>
2021-07-29 14:33:07 -0500

    Commit

--- the-typist/src/components/User/UserRegister.tsx
+++ the-typist/src/components/User/UserRegister.tsx
@@ -1,19 +1,46 @@
-import React from "react";
-import UnloggedBackground from './UnloggedBackground'
+import React, { ChangeEventHandler, MouseEventHandler, useState } from "react";
+import UnloggedBackground from "./UnloggedBackground";
+import "./User.scss";
+import { User } from "../../interfaces/user.interface";
 
-const UserRegister = () =>{
+const UserRegister = () => {
+  const [newUser, setNewUser] = useState<User>({
+    name: "",
+    rol: "USER",
+    password: "",
+  });
 
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-    return (
-        <UnloggedBackground>
-            <div className="container">
-                <div className="header">
-                    Register
-                </div>
-            </div>
-        </UnloggedBackground>
-    );
-}
+  const handleOnChange = (event:any) => {
+    switch(event.target){
+      case 'user':
+
+        break;
+    }
+  }
+
+  return (
+    <UnloggedBackground>
+      <div className="container">
+        <div className="header center">Register</div>
+        <form className="content center flex-1">
+          <div className="group">
+            <label htmlFor="user">User</label>
+            <input id="user" value={newUser.password} onChange={handleOnChange} type="text" />
+          </div>
+          <div className="group">
+            <label htmlFor="password">Password</label>
+            <input id="password" value={newUser.password} onChange={handleOnChange} type="text" />
+          </div>
+          <div className="group">
+            <label htmlFor="confirmPassword">Confirm</label>
+            <input id="confirmPassword" value={confirmPassword} onChange={handleOnChange} type="text" />
+          </div>
+        </form>
+      </div>
+    </UnloggedBackground>
+  );
+};
 
 export default UserRegister;
-
--- the-typist/src/interfaces/user.interface.ts
+++ the-typist/src/interfaces/user.interface.ts
@@ -1,5 +1,6 @@
 export interface User{
     id?:number;
     name:string;
+    rol:string;
     password:string;
 }
