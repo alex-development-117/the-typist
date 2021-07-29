8a6b5e4e87e82a74d1b045b81973eda1ece7f696
Alejandro QF <tnt_games_development@outlook.com>
2021-07-29 13:16:10 -0500

    CRUD user server

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,6 +1,10 @@
 const expressJS = require('express');
 const router = expressJS.Router();
 
+const userController = require('../controllers/userController');
 
+router.get('/get', userController.read);
+router.post('/post', userController.create);
+router.post('/login', userController.login);
 
 module.exports = router;
